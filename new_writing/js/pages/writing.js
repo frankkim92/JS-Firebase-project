b3a953eb7cbce5a19807f4be665b3483b985d679
Dayoung Park <qkrekdud26@gmail.com>
2022-11-25 13:53:42 +0900

    최종 이미지 url 받아서 data create 성공! 해쉬태그 아직 못붙임

--- new_writing/js/pages/writing.js
+++ new_writing/js/pages/writing.js
@@ -1,20 +1,19 @@
 import { authService, storageService, dbService } from "../firebase.js";
+
 import {
   ref,
   uploadString,
   getDownloadURL,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+
 import {
-  doc,
   addDoc,
-  updateDoc,
-  deleteDoc,
   collection,
   orderBy,
   query,
   getDocs,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-// import { updateWrite } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 export const selectEmoji = async (event) => {
@@ -22,43 +21,40 @@ export const selectEmoji = async (event) => {
     let emojiSelect = $('input[name="emoji"]:checked').val();
     console.log(emojiSelect);
   });
-
-  // $("#emojiHappy").prop("checked", true);
-  // $("#emojiHappy").prop("checked", false);
-  // $("#emojiSad").prop("checked", true);
-  // $("#emojiSad").prop("checked", false);
-  // $("#emojiChill").prop("checked", true);
-  // $("#emojiChill").prop("checked", false);
-  // $("#emojiExcited").prop("checked", true);
-  // $("#emojiExcited").prop("checked", false);
-  // $("#emojiTouched").prop("checked", true);
-  // $("#emojiTouched").prop("checked", false);
 };
-
-// let emojiSelect = $("input[name=emoji]:checked").val();
-// console.log(emojiSelect);
-
-// let emojiSelect = document.querySelector('input[name="emoji"]:checked').value;
-
-// const db = firebase.firestore();
 
 export const save_writing = async (event) => {
   event.preventDefault();
-  const coverInput = document.getElementById("coverInput");
+
+  const imgRef = ref(
+    storageService,
+    `${authService.currentUser.uid}/${uuidv4()}`
+  );
+
+  const imgDataUrl = localStorage.getItem("imgDataUrl");
+  // 프로필 이미지 dataUrl을 Storage에 업로드 후 다운로드 링크를 받아서 photoURL에 저장.
+  let downloadUrl;
+  if (imgDataUrl) {
+    const response = await uploadString(imgRef, imgDataUrl, "data_url");
+    downloadUrl = await getDownloadURL(response.ref);
+    // uploadString 으로 firebase 에 이미지 업로드 > response로 성공했는지 확인
+    // imgDataUrl은 임시 이미지 url > 최종 업로드 하면 날라감
+    // response.ref 의 객체를 getDownloadURL로 돌려서 나온 값이 downloadUrl
+  }
+
+  const coverInput = downloadUrl ? downloadUrl : null;
   const artistName = document.getElementById("artistName");
   const songName = document.getElementById("songName");
   const title = document.getElementById("title");
   const hashTag = document.getElementById("hashTag");
   const bodyText = document.getElementById("bodyText");
   const emojiSelect = $('input[name="emoji"]:checked').val();
-  console.log(emojiSelect);
-  // >>??
+  // console.log(emojiSelect);
 
   const { uid, photoURL, displayName } = authService.currentUser;
   try {
     await addDoc(collection(dbService, "Writings"), {
-      coverInput: coverInput.value,
-      // >> photoURL, storage return
+      coverInput: coverInput,
       artistName: artistName.value,
       songName: songName.value,
       emotion: emojiSelect,
@@ -69,6 +65,9 @@ export const save_writing = async (event) => {
       creatorId: uid,
       profileImg: photoURL,
       nickname: displayName,
+    }).then(() => {
+      alert("포스트 업로드 완료");
+      // window.location.hash = "#fanLog";
     });
 
     getWritingList();
@@ -76,6 +75,50 @@ export const save_writing = async (event) => {
     alert(error);
     console.log("error in addDoc:", error);
   }
+};
+
+export const onCoverChange = (event) => {
+  const theCover = event.target.files[0]; // file 객체
+  const reader = new FileReader();
+  // console.log(theCover);
+  reader.readAsDataURL(theCover); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
+  reader.onloadend = (finishedEvent) => {
+    // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
+    const imgDataUrl = finishedEvent.currentTarget.result;
+    localStorage.setItem("imgDataUrl", imgDataUrl);
+    document.getElementById("coverView").src = imgDataUrl;
+  };
+};
+
+export const changeWrite = async (event) => {
+  event.preventDefault();
+  document.getElementById("writeBtn").disabled = true;
+  const imgRef = ref(
+    storageService,
+    `${authService.currentUser.uid}/${uuidv4()}`
+  );
+
+  const imgDataUrl = localStorage.getItem("imgDataUrl");
+  // 프로필 이미지 dataUrl을 Storage에 업로드 후 다운로드 링크를 받아서 photoURL에 저장.
+  let downloadUrl;
+  if (imgDataUrl) {
+    const response = await uploadString(imgRef, imgDataUrl, "data_url");
+    // uploadString 으로 firebase 에 이미지 업로드 > response로 성공했는지 확인
+    // imgDataUrl은 임시 이미지 url > 최종 업로드 하면 날라감
+    downloadUrl = await getDownloadURL(response.ref);
+    // response.ref 의 객체를 getDownloadURL로 돌려서 나온 값이 downloadUrl
+  }
+  await updateWrite(authService.currentUser, {
+    photoURL: downloadUrl ? downloadUrl : null,
+  })
+    .then(() => {
+      alert("게시글 수정 완료");
+      // window.location.hash = "#fanLog";
+    })
+    .catch((error) => {
+      alert("게시글 수정 실패");
+      console.log("error:", error);
+    });
 };
 
 export const getWritingList = async () => {
@@ -126,51 +169,4 @@ export const getWritingList = async () => {
     div.innerHTML = temp_html;
     writingList.appendChild(div);
   });
-};
-
-export const changeWrite = async (event) => {
-  event.preventDefault();
-  document.getElementById("writeBtn").disabled = true;
-  const imgRef = ref(
-    storageService,
-    `${authService.currentUser.uid}/${uuidv4()}`
-  );
-
-  // const newNickname = document.getElementById("profileNickname").value;
-  // >> writing에 해당하는 값으로 변경하기
-  const imgDataUrl = localStorage.getItem("imgDataUrl");
-  // 프로필 이미지 dataUrl을 Storage에 업로드 후 다운로드 링크를 받아서 photoURL에 저장.
-  let downloadUrl;
-  if (imgDataUrl) {
-    const response = await uploadString(imgRef, imgDataUrl, "data_url");
-    // uploadString 으로 firebase 에 이미지 업로드 > response로 성공했는지 확인
-    // imgDataUrl은 임시 이미지 url > 최종 업로드 하면 날라감
-    downloadUrl = await getDownloadURL(response.ref);
-    // response.ref 의 객체를 getDownloadURL로 돌려서 나온 값이 downloadUrl
-  }
-  await updateWrite(authService.currentUser, {
-    // displayName: newNickname ? newNickname : null,
-    photoURL: downloadUrl ? downloadUrl : null,
-  })
-    .then(() => {
-      alert("게시글 수정 완료");
-      // window.location.hash = "#fanLog";
-    })
-    .catch((error) => {
-      alert("게시글 수정 실패");
-      console.log("error:", error);
-    });
-};
-
-export const onCoverChange = (event) => {
-  const theCover = event.target.files[0]; // file 객체
-  const reader = new FileReader();
-  // console.log(theCover);
-  reader.readAsDataURL(theCover); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
-  reader.onloadend = (finishedEvent) => {
-    // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
-    const imgDataUrl = finishedEvent.currentTarget.result;
-    localStorage.setItem("imgDataUrl", imgDataUrl);
-    document.getElementById("coverView").src = imgDataUrl;
-  };
 };
