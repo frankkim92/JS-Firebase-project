cf56b43e5d3e4c69bf4be660816c2389bc577f64
j1young <j1youngyoon94@gmail.com>
2022-11-25 14:55:24 +0900

    add main.js

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ export const date = new Date().toLocaleDateString("en-us", {
 }); //"Tuesday, Nov 22, 2022"
 const today = window.querySelector(".date");
 today.innertext = date;
+
+// url 바뀌면 handleLocation 실행하여 화면 변경
+window.addEventListener("hashchange", handleLocation);
